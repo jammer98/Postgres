543ba457d4f303b8f20301adf64f4347a1a65f5d
Aditya <adityathakur00199.8@gmail.com>
2025-12-19 23:35:59 +0530

    postgress demo project

---
+++ Client/demoo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-neutral-900 h-screen w-full '>
+      
+    </div>
+    
+  )
+}
+
+export default App

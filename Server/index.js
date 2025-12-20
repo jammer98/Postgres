911f3e5009d7ea28b8562a2d67841585af6bf105
Aditya <adityathakur00199.8@gmail.com>
2025-12-20 05:46:25 +0530

    chnaged the .env file and added db_uri inplace of many things

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,16 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+// (async () => {
+//   try {
+//     const result = await pool.query("SELECT 1");
+//     console.log("✅ DB connected:", result.rowCount);
+//   } catch (err) {
+//     console.error("❌ DB connection failed", err);
+//     process.exit(1); // stop app if DB is required
+//   }
+// })();
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
